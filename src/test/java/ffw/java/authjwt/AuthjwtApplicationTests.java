956cb58ffcf1449dbb10cb0084f459926aee9999
Franco Wolff <francofwolff@gmail.com>
2025-04-08 13:18:43 +0000

    Initial commit with Spring Boot project

---
+++ src/test/java/ffw/java/authjwt/AuthjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ffw.java.authjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
